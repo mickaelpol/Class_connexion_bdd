5728638d0d679c344ea93807245e079cc27ba9cc
mickaelpol <polmickael3@gmail.com>
2018-06-06 12:29:15 +0200

    ajout de doc sur autoload.php

--- class/Autoloader.php
+++ class/Autoloader.php
@@ -2,19 +2,33 @@
 
 namespace App;
 
+/**
+ * Class Autoloader à modifer en fonction de ou se trouve vos Class
+ */
 class Autoloader
 {
 
+    /**
+     * Retourne la fonction autoload
+     *
+     */
     static function register()
     {
         spl_autoload_register(array(__CLASS__, 'autoload'));
     }
     
+    /**
+     * Lance la fonction autoload allant chercher les class dans le bon dossier et le bon fichier
+     * à modifier selon vos spécificités
+     *
+     * @param string $class
+     * @return void
+     */
     static function autoload($class)
     {
         $class = str_replace(__NAMESPACE__."\\", "", $class);
         $class = str_replace('\\', '/', $class);
-        require 'class/' . $class . '.php';
+        require '../class/' . $class . '.php';
     }
     
 }
